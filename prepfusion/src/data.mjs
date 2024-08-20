6228367beb10807ebc9a032e656b023bbf0d84d8
Aditya <therealaditya25@gmail.com>
2024-08-20 10:05:22 +0530

    Add files via upload

---
+++ prepfusion/src/data.mjs
@@ -0,0 +1,14 @@
+const dataa = {
+    "createuser" : "https://prepfusion-darshil.onrender.com/auth/createuser",
+    "loginuser" : "https://prepfusion-darshil.onrender.com/auth/login",
+    "userdata" : "https://prepfusion-darshil.onrender.com/auth/getuser",
+    "pay_orderdetail" : "https://prepfusion-darshil.onrender.com/payment/orders",
+    "pay_verify" : "https://prepfusion-darshil.onrender.com/payment/verify",
+    "pay_updatePremium" : "https://prepfusion-darshil.onrender.com/payment/status",
+    "bookmark" : "https://prepfusion-darshil.onrender.com/question/bookmark",
+    "allbookmarks" : "https://prepfusion-darshil.onrender.com/question/fetchallbookmarks"
+
+};
+
+export default dataa;
+
